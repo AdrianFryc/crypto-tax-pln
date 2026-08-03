b559364fdfce6cab83d537b9f3d0b448344640ab
Adrian Fryc <adrian.fryc@gmail.com>
2026-08-03 16:19:14 +0200

    Init commit: Spring Boot setup

---
+++ src/main/java/pl/cryptotax/CryptoTaxPlnApplication.java
@@ -0,0 +1,13 @@
+package pl.cryptotax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoTaxPlnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoTaxPlnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/cryptotax/CryptoTaxPlnApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.cryptotax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoTaxPlnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
